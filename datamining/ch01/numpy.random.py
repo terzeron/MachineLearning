32016dd37805dd00d3e706480e8e544e1253053d
Youngil Cho <terzeron@gmail.com>
2025-10-01 13:26:49 +0900

    modify old codes according to recent version module

--- datamining/ch01/numpy.random.py
+++ datamining/ch01/numpy.random.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-from numpy import *
+from numpy import bmat, random, eye
 
 print(random.rand(4,4))
-randMat = mat(random.rand(4, 4))
+randMat = bmat(random.rand(4, 4))
 print(randMat)
 invRandMat = randMat.I
 print(invRandMat)
